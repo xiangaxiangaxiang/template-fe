57d85e1e448fd1ce54ab8707cb3f0bef5fecadb6
cpx <872632899@qq.com>
2023-05-19 15:01:55 +0800

    feat: 添加项目包

---
+++ packages/shared/src/components/pro-table/index.ts
@@ -0,0 +1,1 @@
+export { default as ProTable } from './index.vue'
---
+++ packages/shared/src/components/pro-table/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="pro-table"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less"></style>
